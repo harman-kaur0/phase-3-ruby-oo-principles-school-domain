8bcbf986bc506b73c07425f7e82d7ed2a0d061a7
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-19 11:39:20 -0700

    commit tree fb72dffea9e31e327f93949b73209b2db0e8b9aa

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end   
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+            @roster[grade] << student_name
+        elsif !@roster[grade]
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+      @roster.each {|grade, students| @roster[grade] = students.sort}
+    end
+end
